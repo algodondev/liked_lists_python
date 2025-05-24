306cee200cf72f3b800738baa7ecbe9162eb0ff5
algodondev <179851883+algodondev@users.noreply.github.com>
2025-05-23 18:11:40 -0600

    second commit

--- ClassLinkedList.py
+++ ClassLinkedList.py
@@ -27,7 +27,7 @@ class LinkedList:
         current = self.get_first()
 
         while(current != None):
-            if(currentIndex == index): return current.get_data()
+            if(currentIndex == index): return current
             currentIndex += 1
             current = current.get_next()
         
@@ -61,27 +61,97 @@ class LinkedList:
         aux.set_next(self.get_first())
         self.set_first(aux)
 
-    # Insert at a specific position
+    # # Insert at a specific position
     def insert_at(self, value, pos):
         aux = Node(value)
-        # Si la lista esta vacia
-        if self.is_empty():
-            self.set_first(aux)
-            self.set_last(aux)
+        if self.is_empty(): 
+            self.first = aux
+            self.last = aux
             return
         
         if pos <= 0:
             self.add_to_start(value)
             return
         
-        n1 = self.find_at(pos -1)
+        # Si la lista no esta vacia
+        n1 = self.find_at(pos - 1)
         if n1 == None:
             self.add(value)
             return
         
         n2 = n1.get_next()
-        aux.get_next() = n2
-        n1.get_next() = aux
+        aux.set_next(n2)
+        n1.set_next(aux)
+
+    #devuelve el indice de la primera ocurrencia de un valor
+    def find(self, value):
+        if self.is_empty():
+            return -1
+        
+        current = self.get_first()
+        index = 0
+
+        while True:
+            if current.get_data() == value: return index
+            current = current.get_next()
+            index +=1 
+            if current == None: break
+        
+        return -1
+    
+    #Longitud de la lista
+    def length(self):
+        if self.is_empty(): return 0
+        counter = 0
+        current = self.get_first()
+
+        while True:
+            counter+=1
+            current = current.get_next()
+            if current == None: break
+
+        return counter
+
+    #Eliminar primer dato
+    def delete_first(self):
+        if self.is_empty(): return
+        aux = self.get_first()
+        self.set_first(self.get_first().get_next()) 
+
+        aux.set_next(None)
+
+    #Eliminar el ultimo dato
+    def delete_last(self):
+        if self.is_empty(): return 
+        if self.length() == 1:
+            self.delete_first()
+            return
+        
+        self.set_last(self.find_at(self.length() - 2))
+        self.get_last().set_next(None)
+                      
+    # eliminar en una posicion especifica
+    def delete_at(self, pos):
+        if self.is_empty(): return
+        if pos < 0 or pos >= self.length(): return
+        if pos == 0:
+            self.delete_first()
+            return
+        
+        aux = self.find_at(pos)
+        nxt = aux.get_next()
+        prev = self.find_at(pos - 1)
+        prev.set_next(nxt)
+        aux.set_next(None)
+
+    def delete_by(self, value):
+        if self.is_empty(): return
+
+        if self.find(value) >= 0:
+            self.delete_at(self.find(value))
+        else:
+            return
+
 
     # Mostrar la lista
     def show(self):
@@ -91,8 +161,11 @@ class LinkedList:
 
         current = self.get_first()
 
+        message = ''
         while True:
-            print(f"{current.get_data()}")
+            message = message + f"-> {current.get_data()} "
             current = current.get_next()
             if current == None: break
+
+        print(message)
     
--- main.py
+++ main.py
@@ -1,16 +1,117 @@
 
+import os
 from ClassLinkedList import LinkedList
+
+def clear_console():
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # Linux, macOS, etc.
+        os.system('clear')
 
 lista = LinkedList()
 
-lista.add(10)
-lista.add(1)
-lista.add(14)
-lista.add_to_start(4)
+while True:
+    
+    clear_console()
+    opcion = input("Menu de Linked Lists\n" \
+    "a. Mostrar lista\n" \
+    "b. Insertar al final\n" \
+    "c. Insertar al inicio\n" \
+    "d. Insertar en una posicion especifica\n" \
+    "e. Eliminar al final\n" \
+    "f. Eliminar al inicio\n" \
+    "g. Eliminar de una posicion especifica\n" \
+    "h. Eliminar por valor\n" \
+    "i. Buscar\n" \
+    "j. SALIR\n")
 
-lista.show()
+    match opcion:
+        case "a":
+            clear_console()
+            if(lista.length() > 0):
+                lista.show()
+            else:
+                print("La lista esta vacia")
 
-index = int(input("Ingrese el indice: "))
-valor = lista.find_at(index)
+            input("\nPresiona cualquier tecla para continuar... ")
+        case "b":
+            clear_console()
+            valor = int(input("Valor a insertar: "))
+            lista.add(valor)
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
 
-print(f"En la posicion {index} esta {valor}")
+        case "c":
+            clear_console()
+            valor = int(input("Valor a insertar: "))
+            lista.add_to_start(valor)
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+
+        case "d":
+            clear_console()
+            valor = int(input("Valor a insertar: "))
+            pos = int(input("Posicion donde insertar: "))
+            lista.insert_at(valor, pos)
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+
+        case "e":
+            clear_console()
+            lista.delete_last()
+            print("Ultimo valor eliminado")
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+
+        case "f":
+            clear_console()
+            lista.delete_first()
+            print("Primer valor eliminado")
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+        case "g":
+            clear_console()
+            pos = int(input("Posicion a eliminar: "))
+            lista.delete_at(pos)
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+
+        case "h":
+            clear_console()
+            valor = int(input("Valor a eliminar: "))
+            lista.delete_by(valor)
+            print("\nNuevos valores de lista: \n")
+            lista.show()
+            input("\nPresiona cualquier tecla para continuar...")
+
+        case "i":
+            clear_console()
+            op = int(input("Seleccione una opcion\n" \
+            "\n1. Buscar por valor\n" \
+            "2. Buscar por posicion\n"))
+
+            match op:
+                case 1:
+                    val = int(input("Inserte el valor a buscar: "))
+                    print(f"El valor se encuentra en la posicion {lista.find(val)}")
+                case 2:
+                    ind = int(input("Inserte la posicion a buscar: "))
+                    if ind >= lista.length(): 
+                        print("Posicion no valida")
+                    else:
+                        print(f"La posicion {ind} contiene el valor {lista.find_at(ind).get_data()}")
+                case _:
+                    input("Opcion no disponible, presiona cualquier tecla para volver al menu principal...")
+            input("Presiona cualquier tecla para volver al menu principal... ")
+
+        case "j":
+            break
+        case _:
+            print("Opcion no disponible")
+    
